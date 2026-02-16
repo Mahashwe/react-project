6f9835cae13119ecd4ab26e66df3c2e4a2bad765
Mahashwe <maha.tomholland@gmail.com>
2026-02-16 12:29:15 +0530

    feat: add routing with react-router-dom and enhance navigation in the app

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Head from './Components/title';
 import Main from './Components/main';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Head />
-      <Main />
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/progress" element={<h2>Dashboard page</h2>} />
+      </Routes>
+    </>
   );
 }
 
--- todo-app/src/Components/title.tsx
+++ todo-app/src/Components/title.tsx
@@ -1,12 +1,18 @@
-import './style.css'
+import "./style.css";
+import { Link } from "react-router-dom";
 
 export const Head = () => {
     return (
         <div className="title">
+            <div className="nav">
+                <Link to="/">Home</Link>
+                <Link to="/progress">Progress</Link>
+            </div>
+
             <h1>ToDo App</h1>
-            <p>Your all needs in one place </p>
+            <p>Your all needs in one place</p>
         </div>
-    )
-}
+    );
+};
 
 export default Head;
